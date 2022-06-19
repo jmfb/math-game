1411c3802b4a3cb967bcd1bde47758daecdd5dc2
Jacob Buysse <Jacob.Buysse@gmail.com>
2022-06-19 16:35:58 -0500

    Adding dev server setup.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+const AsyncRoot = React.lazy(
+	() => import(/* webpackChunkName: 'Root' */ './Root')
+);
+
 function main() {
 	const rootElement = document.getElementById('root');
 	if (!rootElement) {
 		throw new Error('Missing root DOM element.');
 	}
 
-	const rootComponent = <div>Hello World</div>;
-	createRoot(rootElement).render(rootComponent);
+	createRoot(rootElement).render(<AsyncRoot />);
 }
 
 main();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,10 +3,15 @@ const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 const EsLintPlugin = require('eslint-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
+const fs = require('fs');
 
 const treatWarningsAsErrors = !process.argv.includes('--watch');
+const version = fs.readFileSync('./src/version.json', 'utf8').trim();
 
 module.exports = {
 	mode: 'production',
@@ -55,18 +60,73 @@ module.exports = {
 		new MiniCssExtractPlugin({
 			filename: '[name].css?[contenthash]'
 		}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					context: path.join(__dirname, 'src'),
+					from: 'version.json'
+				}
+			]
+		}),
 		new EsLintPlugin({
 			extensions: ['ts', 'tsx'],
 			failOnWarning: treatWarningsAsErrors,
 			exclude: ['node_modules']
 		}),
-		new ForkTsCheckerWebpackPlugin()
+		new ForkTsCheckerWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Math Games',
+			favicon: './src/favicon.ico',
+			meta: {
+				viewport:
+					'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+				author: 'Jacob Buysse',
+				description: 'Math Games'
+			},
+			inject: false,
+			templateContent: ({ htmlWebpackPlugin: { tags } }) => `
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		${tags.headTags}
+	</head>
+	<body>
+		<div id="root" data-bundle-version="${version}"></div>
+	</body>
+</html>
+`
+		}),
+		new CleanWebpackPlugin()
 	],
 	optimization: {
 		minimize: true,
-		minimizer: [new TerserWebpackPlugin(), new CssMinimizerWebpackPlugin()],
+		minimizer: [
+			new TerserWebpackPlugin({
+				terserOptions: {
+					output: {
+						comments: false
+					}
+				},
+				extractComments: false
+			}),
+			new CssMinimizerWebpackPlugin()
+		],
 		splitChunks: {
 			chunks: 'all'
 		}
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, './build')
+		},
+		client: {
+			overlay: {
+				errors: true,
+				warnings: false
+			}
+		},
+		compress: true,
+		port: 6402,
+		historyApiFallback: true
 	}
 };
