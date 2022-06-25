49b7ac93f98a3e9c365b70f5245a6f14e1680fc3
Jacob Buysse <Jacob.Buysse@gmail.com>
2022-06-25 11:26:28 -0500

    Filling out about page.

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
 export function About() {
-	return <div>TODO: About</div>;
+	const projectHref = 'https://github.com/jmfb/math-game';
+	return (
+		<section>
+			<h1>About Math Games</h1>
+			<p>
+				Math Games was inspired by Mya's love for math and quest to
+				continue learning even when not at school.
+			</p>
+			<p>The source code for this project can be found here:</p>
+			<ul>
+				<li>
+					<a href={projectHref}>{projectHref}</a>
+				</li>
+			</ul>
+		</section>
+	);
 }
